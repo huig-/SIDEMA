71ff2f2c82c4fe6be43316d9c93752bb22e7eace
Miguel Isabel <miguelis@ucm.es>
2016-04-21 18:37:22 +0200

    Añadiendo un nuevo neutralizador más y comenzando sincronización entre CC y neutralizadores

--- src/icaro/aplicaciones/SIDEMA/informacion/CentroControl.java
+++ src/icaro/aplicaciones/SIDEMA/informacion/CentroControl.java
@@ -3,6 +3,8 @@
  */
 package icaro.aplicaciones.SIDEMA.informacion;
 
+import java.util.ArrayList;
+
 /**
  * @author Mike
  *
@@ -15,9 +17,27 @@ public class CentroControl extends Robot {
 	 * @param energy
 	 * @param leader
 	 */
+	private ArrayList<String> exploradores;
+	private ArrayList<String> neutralizadores;
 	public CentroControl(String id, Celda currentPos, int energy, String leader) {
 		super(id, currentPos, energy, leader);
-		// TODO Auto-generated constructor stub
+		this.exploradores = new ArrayList<String>();
+		this.neutralizadores = new ArrayList<String>();
+		this.neutralizadores.add("agenteNeutralizador0");
+		this.neutralizadores.add("agenteNeutralizador1");
+	}
+	
+	public ArrayList<String> getExploradores() {
+		return exploradores;
+	}
+	public void setExploradores(ArrayList<String> exploradores) {
+		this.exploradores = exploradores;
+	}
+	public ArrayList<String> getNeutralizadores() {
+		return neutralizadores;
+	}
+	public void setNeutralizadores(ArrayList<String> neutralizadores) {
+		this.neutralizadores = neutralizadores;
 	}
 
 }
--- src/icaro/aplicaciones/agentes/agenteCC/tareas/EnviarNeutralizador.java
+++ src/icaro/aplicaciones/agentes/agenteCC/tareas/EnviarNeutralizador.java
@@ -1,9 +1,11 @@
 package icaro.aplicaciones.agentes.agenteCC.tareas;
 
 import icaro.aplicaciones.SIDEMA.informacion.Celda;
+import icaro.aplicaciones.SIDEMA.informacion.CentroControl;
 import icaro.aplicaciones.SIDEMA.informacion.Mapa;
 import icaro.aplicaciones.SIDEMA.informacion.OrdenDesactivar;
 import icaro.aplicaciones.SIDEMA.informacion.OrdenExplorar;
+import icaro.aplicaciones.SIDEMA.informacion.Robot;
 import icaro.infraestructura.entidadesBasicas.procesadorCognitivo.TareaSincrona;
 
 public class EnviarNeutralizador extends TareaSincrona {
@@ -12,9 +14,10 @@ public class EnviarNeutralizador extends TareaSincrona {
 	public void ejecutar(Object... params) {
 		try {
 			Celda c = (Celda)params[0];
+			CentroControl r = (CentroControl)params[1];
 			//Elegir...OrdenElegirNeutralizador orden = new OrdenElegirNeutralizador("CC", m);
 			OrdenDesactivar orden = new OrdenDesactivar("agenteCC",c);
-			this.getComunicator().enviarInfoAotroAgente(orden, "agenteNeutralizador0"); //1 es el identificador del neutralizador
+			this.getComunicator().enviarInfoAotroAgente(orden,r.getNeutralizadores().get(0)); //1 es el identificador del neutralizador
 		}
 		catch (Exception e) {
 			e.printStackTrace();
