9d31a2d5a9a6170c3b1cccbf25766d2e272463d5
tutugordillo <tutugordillo@gmail.com>
2016-04-28 14:29:44 +0200

    tarea CC obtener mapa

---
+++ src/icaro/aplicaciones/agentes/agenteCC/tareas/ObtenerMapa.java
@@ -0,0 +1,49 @@
+package icaro.aplicaciones.agentes.agenteCC.tareas;
+
+import java.io.File;
+
+import icaro.aplicaciones.SIDEMA.informacion.Mapa;
+import icaro.aplicaciones.recursos.recursoPersistenciaSIDEMA.ItfUsoRecursoPersistenciaSIDEMA;
+import icaro.aplicaciones.recursos.recursoVisualizacionSIDEMA.ItfUsoRecursoVisualizacionSIDEMA;
+import icaro.infraestructura.entidadesBasicas.NombresPredefinidos;
+import icaro.infraestructura.entidadesBasicas.procesadorCognitivo.TareaSincrona;
+
+public class ObtenerMapa extends TareaSincrona{
+
+	@Override
+	public void ejecutar(Object... params) {
+		// TODO Auto-generated method stub
+		File fichero;
+		Mapa m;
+		try {
+			ItfUsoRecursoVisualizacionSIDEMA visualizador = (ItfUsoRecursoVisualizacionSIDEMA)
+					NombresPredefinidos.REPOSITORIO_INTERFACES_OBJ.obtenerInterfaz(
+							NombresPredefinidos.ITF_USO + "RecursoVisualizacionSIDEMA1");
+			
+			ItfUsoRecursoPersistenciaSIDEMA persistencia = (ItfUsoRecursoPersistenciaSIDEMA)
+					NombresPredefinidos.REPOSITORIO_INTERFACES_OBJ.obtenerInterfaz(
+							NombresPredefinidos.ITF_USO + "RecursoPersistenciaSIDEMA1");
+			
+			if(!visualizador.getMapaPintado()){
+				visualizador.mostrarEntornoSimulacion();
+				fichero = visualizador.getFicheroEscenario();
+				persistencia.setFicheroEscenario(fichero);
+				persistencia.parserCSVModelo();
+				m = persistencia.getEscenario();
+				visualizador.setMapa(m);
+				visualizador.pintarEscenario();
+				
+				
+			}
+			else{
+				m = visualizador.getMapa();
+				persistencia.setEscenario(m);
+			}
+		} catch (Exception e) {
+			 this.trazas.mostrarMensajeError("Error en la tarea ObtenerMapa. Mensaje: "+e.getMessage());
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
